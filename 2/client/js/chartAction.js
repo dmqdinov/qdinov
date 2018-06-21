663f46c570fb3efc5280edffb407e8fa94978a2f
dmqdinov <40473780+dmqdinov@users.noreply.github.com>
2018-06-21 22:34:03 +0300

    Add files via upload

---
+++ 2/client/js/chartAction.js
@@ -0,0 +1,205 @@
+var encodesBarBy = "Population";
+var initialData;
+var selectedYear;
+var dataForCurrentYear;
+var displayedData;
+var aggrBy = "Country";
+var sortBy = "Name";
+
+var max;
+var min;
+
+function initBarPage(data) {
+
+  initialData = data;
+
+  var range = getMaxAndMinYears(initialData);
+  selectedYear = ~~((range[1] + range[0]) / 2);
+  d3.select("input[name='range']")
+    .attr("min", range[0])
+    .attr("max", range[1])
+    .attr("value", selectedYear)
+    .on("change", changeBarsYear);
+
+    // записываем данные о текущих годах в label
+  d3.select("label[name ='min']").text(range[0]);
+  d3.select("label[name ='max']").text(range[1]);
+  d3.select("label[name ='now']").text(selectedYear);
+
+  d3.selectAll("input[name='encodesBarRadio']").on("change", changeEncode);
+  d3.selectAll("input[name='checkbox']").on("change", barFilter);
+  d3.selectAll("input[name='aggrRadio']").on("change", barAggregate);
+  d3.selectAll("input[name='sortRadio']").on("change", sortRadio);
+
+  renderBarPage();
+}
+
+function renderBarPage() {
+  d3.select("g").remove();
+
+  var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  dataForCurrentYear = getFilteredInfoForCurrentYear(initialData, selectedYear);
+
+  displayedData = dataForCurrentYear;
+  if (encodesBarBy == "Population") {
+    max = d3.max(displayedData, function(d) {
+      return d.population;
+    });
+  } else {
+    max = d3.max(displayedData, function(d) {
+      return d.gdp;
+    });
+  }
+
+  min = 0;
+  
+  // считаем scale, знаем что 118 элементов норм влезают и отступы ок
+  var heightScale = displayedData.length / 118;
+  
+  // да бы размер svg элемента менялся каждый раз при пересчете
+  // записываем тут максимальный height помноженный на scale
+  d3.select("svg").attr("height", 1800 * heightScale + margin.top + margin.bottom );
+
+  xScale.domain([min, max]);
+  yScale.domain(dataForCurrentYear.map(function(d) {
+    return d.name;
+  }));
+
+  var groups = g.append("g")
+    .selectAll("text")
+    .data(displayedData)
+    .enter()
+    .append("g");
+
+  var labels = groups
+    .append("text")
+    .text(function(d) {
+      return d.name
+    })
+    .attr("x", xScale(min))
+    .attr("y", function(d) {
+      return yScale(d.name) * 2 * heightScale; // чтоб отступы сверху были норм, тоже помнажаю на scale 
+    })
+    .attr("dy", "0.7em")
+    .attr("dx", "-11em")
+    .attr("class", "label");
+
+  var bars = groups
+    .append("rect")
+    .attr("width", function(d) {
+      if (encodesBarBy == "Population") {
+        return xScale(d.population);
+      } else {
+        return xScale(d.gdp)
+      }
+    })
+    .attr("x", xScale(min))
+    .attr("y", function(d) {
+      return yScale(d.name) * 2 * heightScale;  // и тут
+    })
+    .attr("class", "bar");
+}
+
+function barFilter() {
+  renderBarPage();
+}
+
+
+function barAggregate() {
+  aggrBy = d3.select("input[name='aggrRadio']:checked").property("value");
+  renderBarPage();
+}
+
+function changeEncode() {
+  encodesBarBy = d3.select("input[name='encodesBarRadio']:checked").property("value");
+  renderBarPage();
+}
+
+function sortRadio() {
+  sortBy = d3.select("input[name='sortRadio']:checked").property("value");
+  renderBarPage();
+}
+
+function changeBarsYear() {
+  selectedYear = d3.select("input[name='range']").property("value");
+  // когда обновим дату, то и тут тоже надо перезаписать значение
+  d3.select("label[name ='now']").text(selectedYear);
+  renderBarPage();
+}
+
+
+function getFilteredInfoForCurrentYear(data, year) {
+  var newData = [];
+  for (var i = 0; i < data.length; i++) {
+    var currentYears = data[i].years;
+    var j = 0;
+    while (currentYears[j].year != year) {
+      j++;
+    }
+    getCheckedContinents();
+    if (aggrBy == "Country") {
+      if (!Array.isArray(checkedContinents) || !checkedContinents.length) {
+        newData[i] = {
+          alpha2_code: data[i].alpha2_code,
+          continent: data[i].continent,
+          gdp: !!(data[i].years)[j].gdp ? (data[i].years)[j].gdp : "",
+          latitude: data[i].latitude,
+          life_expectancy: (data[i].years)[j].life_expectancy,
+          longitude: data[i].longitude,
+          name: data[i].name,
+          population: (data[i].years)[j].population,
+          year: (data[i].years)[j].year
+        }
+      } else {
+        if (checkedContinents.indexOf(data[i].continent) > -1) {
+          newData.push({
+            alpha2_code: data[i].alpha2_code,
+            continent: data[i].continent,
+            gdp: !!(data[i].years)[j].gdp ? (data[i].years)[j].gdp : "",
+            latitude: data[i].latitude,
+            life_expectancy: (data[i].years)[j].life_expectancy,
+            longitude: data[i].longitude,
+            name: data[i].name,
+            population: (data[i].years)[j].population,
+            year: (data[i].years)[j].year
+          });
+        }
+      }
+    } else {
+      newData = getAggregatedByContinentsInfo(dataForCurrentYear);
+    }
+  }
+  return sortData(newData, sortBy);
+}
+
+
+function sortData(data, sortByParam) {
+  switch (sortByParam) {
+    case "Name":
+      data.sort(sortByKey("name"));
+      break;
+    case "Population":
+      data.sort(sortByKey("population"));
+      break;
+    case "GDP":
+      data.sort(sortByKey("gdp"));
+      break;
+  }
+  return data;
+}
+
+function sortByKey(key, reverse) {
+  var moveSmaller = reverse ? 1 : -1;
+  var moveLarger = reverse ? -1 : 1;
+  return function(a, b) {
+    if (a[key] < b[key]) {
+      return moveSmaller;
+    }
+    if (a[key] > b[key]) {
+      return moveLarger;
+    }
+    return 0;
+  };
+}
---
+++ 2/client/js/tableAction.js
@@ -0,0 +1,185 @@
+var desiredCols = ["name", "continent", "gdp", "life_expectancy", "population", "year"];
+
+function getDesiredCols() {
+  return desiredCols;
+}
+
+function format(column, value) {
+  switch (column) {
+    case "name":
+      return value;
+    case "continent":
+      return value;
+    case "gdp":
+      return d3.format("8,.1f")(value / 10e9) + "G";
+    case "life_expectancy":
+      return d3.format("8,.1f")(value);
+    case "population":
+      return d3.format(",")(value);
+    case "year":
+      return (value);
+    default:
+      alert("Something wrong!");
+  }
+}
+
+function filter() {
+
+  getCheckedContinents();
+
+  if (!Array.isArray(checkedContinents) || !checkedContinents.length) {
+    tbody.selectAll("tr.row").each(function() {
+      d3.select(this).classed("filtered", false);
+    });
+  } else {
+    tbody.selectAll("tr.row").each(function() {
+      d3.select(this).classed("filtered", true);
+    });
+
+    tbody.selectAll("tr.row").each(function() {
+      var nrow = (d3.select(this))[0][0].innerText;
+      for (var j = 0; j < checkedContinents.length; j++) {
+        if (nrow.indexOf(checkedContinents[j]) > -1) {
+          d3.select(this).classed("filtered", false);
+        }
+      }
+    });
+  }
+}
+
+
+function aggregate() {
+  var aggr = d3.select('input[name="radio"]:checked').property("value");
+  if (aggr == "None") {
+    tbody.selectAll("tr.row").each(function() {
+      var nrow = (d3.select(this))[0][0].innerText;
+
+      var applyFilterStyle = false;
+      for (var i = 0; i < continents.length; i++) {
+        if (getNumOfOccur(nrow, continents[i]) == 2) {
+          applyFilterStyle = true;
+          break;
+        }
+      }
+      d3.select(this).classed("filtered", applyFilterStyle);
+    });
+  } else {
+    tbody.selectAll("tr.row").each(function() {
+      var nrow = (d3.select(this))[0][0].innerText;
+
+      var applyFilterStyle = true;
+      for (var i = 0; i < continents.length; i++) {
+        if (getNumOfOccur(nrow, continents[i]) == 2) {
+          applyFilterStyle = false;
+          break;
+        }
+      }
+      d3.select(this).classed("filtered", applyFilterStyle);
+    });
+  }
+}
+
+
+function getNumOfOccur(str, pattern) {
+  var pos = str.indexOf(pattern);
+  for (var count = 0; pos != -1; count++)
+    pos = str.indexOf(pattern, pos + pattern.length);
+  return count;
+}
+
+
+function init(data) {
+  initialData = data;
+
+  d3.selectAll("input[name='checkbox']").on("change", filter);
+  d3.selectAll("input[name='radio']").on("change", aggregate);
+
+  var range = getMaxAndMinYears(initialData);
+  selectedYear = ~~((range[1] + range[0]) / 2);
+  d3.select("input[name='range']")
+    .attr("min", range[0])
+    .attr("max", range[1])
+    .attr("value", selectedYear)
+    .on("change", changeYear);
+
+  d3.select("label[name ='min']").text(range[0]);
+  d3.select("label[name ='max']").text(range[1]);
+
+
+  render();
+}
+
+function changeYear() {
+  selectedYear = d3.select("input[name='range']").property("value");
+  render();
+}
+
+function render() {
+  d3.select("table").remove();
+
+  var columns = getDesiredCols();
+
+  var table = d3.select("body").append("table"),
+    thead = table.append("thead")
+    .attr("class", "thead");
+  tbody = table.append("tbody");
+
+  filter();
+  aggregate();
+
+  table.append("caption")
+    .html("World Countries Ranking");
+
+  thead.append("tr").selectAll("th")
+    .data(columns)
+    .enter()
+    .append("th")
+    .text(function(d) {
+      return d;
+    })
+    .on("click", function(header) {
+      var i = 0;
+      tbody.selectAll("tr").sort(function(a, b) {
+        if (sortFlag <= 0) {
+          i = 1;
+          return d3.ascending(a[header], b[header]);
+        } else {
+          i = -1;
+          return d3.descending(a[header], b[header]);
+        }
+      });
+      sortFlag = i;
+    });
+
+  var dataForCurrrentYear = getInfoForCurrentYear(initialData, selectedYear);
+  var aggr = getAggregatedByContinentsInfo(dataForCurrrentYear);
+  var rows = tbody.selectAll("tr.row")
+    .data(dataForCurrrentYear.concat(aggr))
+    .enter()
+    .append("tr").attr("class", "row");
+
+  var cells = rows.selectAll("td")
+    .data(function(row) {
+      return d3.range(columns.length).map(function(column, i) {
+        return format(columns[i], row[columns[i]])
+      });
+    })
+    .enter()
+    .append("td")
+    .text(function(d) {
+      return d;
+    })
+    .on("mouseover", function(d, i) {
+
+      d3.select(this.parentNode)
+        .style("background-color", "#F3ED86");
+
+    }).on("mouseout", function() {
+
+      tbody.selectAll("tr")
+        .style("background-color", null)
+        .selectAll("td")
+        .style("background-color", null);
+    });
+
+}
